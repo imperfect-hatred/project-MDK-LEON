39b47cddc1e07f77a7f14cf876a42916235f49f0
imperfect-hatred <nnikakoj3@gmail.com>
2024-11-11 13:26:19 +0900

    add slider and picture

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,11 @@
 const burgerButton = document.querySelector(".burger");
 const overlay = document.querySelector(".desktop__header");
-const menu = document.querySelector(".desktop__header");
+const exitButton = document.querySelector(".exit");
 
 burgerButton.addEventListener("click", () => {
   overlay.classList.add("overlay__open");
+});
+
+exitButton.addEventListener("click", () => {
+  overlay.classList.remove("overlay__open");
 });
