3936bba149549f0f71583b37b5c980761e86e5af
imperfect-hatred <nnikakoj3@gmail.com>
2024-12-11 14:07:47 +0900

    fix finally

--- scripts/main.js
+++ scripts/main.js
@@ -30,29 +30,65 @@ const exitPhone = document.querySelector(".exit-phone");
 const chatButton = document.querySelector(".chat-button");
 const overlayChat = document.querySelector(".chat");
 const exitChat = document.querySelector(".exit-chat");
+const readMoreLink = document.getElementById("readMoreLink");
+const readLessLink = document.getElementById("readLessLink");
+const overlay = document.querySelector(".opacity-background");
+const navItems = document.querySelectorAll(".desktop__header--mid li a");
+
+/* adding class active on click menu item */
+
+navItems.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    navItems.forEach((el) => {
+      el.classList.remove("link__active");
+    });
+    item.classList.add("link__active");
+  });
+});
 
 burgerButton.addEventListener("click", () => {
   overlayBurger.classList.add("overlay__open");
+  overlay.classList.add("active");
 });
 
 exitButton.addEventListener("click", () => {
   overlayBurger.classList.remove("overlay__open");
+  overlay.classList.remove("active");
 });
 
 phoneButton.addEventListener("click", () => {
   overlayPhone.classList.add("show");
+  overlay.classList.add("active");
 });
 
 exitPhone.addEventListener("click", () => {
   overlayPhone.classList.remove("show");
-  document.body.style.overflow = "auto";
+  overlayBurger.classList.remove("overlay__open");
+  overlay.classList.remove("active");
 });
 
 chatButton.addEventListener("click", () => {
   overlayChat.classList.add("show");
+  overlay.classList.add("active");
 });
 
 exitChat.addEventListener("click", () => {
   overlayChat.classList.remove("show");
-  document.body.style.overflow = "auto";
+  overlayBurger.classList.remove("overlay__open");
+  overlay.classList.remove("active");
+});
+
+readMoreLink.addEventListener("click", function (event) {
+  event.preventDefault();
+  moreText.style.display = "flex";
+  moreText.style.flexDirection = "column";
+  readMoreLink.style.display = "none";
+  readLessLink.style.display = "flex";
+});
+
+readLessLink.addEventListener("click", function (event) {
+  event.preventDefault();
+  moreText.style.display = "none";
+  readMoreLink.style.display = "flex";
+  readLessLink.style.display = "none";
 });
