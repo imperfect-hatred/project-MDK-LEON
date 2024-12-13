7297690efbdb3ba08c3f703f5dcee6bd8caa6cfe
imperfect-hatred <nnikakoj3@gmail.com>
2024-12-13 22:08:44 +0900

    itog

--- scripts/main.js
+++ scripts/main.js
@@ -24,14 +24,22 @@ const swiper = new Swiper(".swiper", {
 const burgerButton = document.querySelector(".burger");
 const overlayBurger = document.querySelector(".desktop__header");
 const exitButton = document.querySelector(".exit");
-const phoneButton = document.querySelector(".phone-button");
+const phoneButton = document.querySelectorAll(".phone-button");
 const overlayPhone = document.querySelector(".phone");
-const exitPhone = document.querySelector(".exit-phone");
-const chatButton = document.querySelector(".chat-button");
+const exitPhone = document.querySelectorAll(".exit-phone");
+const chatButton = document.querySelectorAll(".chat-button");
 const overlayChat = document.querySelector(".chat");
-const exitChat = document.querySelector(".exit-chat");
+const exitChat = document.querySelectorAll(".exit-chat");
 const readMoreLink = document.getElementById("readMoreLink");
 const readLessLink = document.getElementById("readLessLink");
+const readMoreLink_swiper = document.getElementById("readMoreLink_swiper");
+const readLessLink_swiper = document.getElementById("readLessLink_swiper");
+const readMoreLink_swiper_other = document.getElementById(
+  "readMoreLink_swiper_other"
+);
+const readLessLink_swiper_other = document.getElementById(
+  "readLessLink_swiper_other"
+);
 const overlay = document.querySelector(".opacity-background");
 const navItems = document.querySelectorAll(".desktop__header--mid li a");
 
@@ -46,6 +54,38 @@ navItems.forEach((item) => {
   });
 });
 
+readMoreLink_swiper_other.addEventListener("click", function (event) {
+  event.preventDefault();
+  readMoreLink_swiper_other.style.display = "none";
+  readLessLink_swiper_other.style.display = "flex";
+});
+
+readLessLink_swiper_other.addEventListener("click", function (event) {
+  event.preventDefault();
+  readLessLink_swiper_other.style.display = "none";
+  readMoreLink_swiper_other.style.display = "flex";
+});
+
+readMoreLink_swiper.addEventListener("click", function (event) {
+  event.preventDefault();
+  readMoreLink_swiper.style.display = "none";
+  readLessLink_swiper.style.display = "flex";
+});
+
+readLessLink_swiper.addEventListener("click", function (event) {
+  event.preventDefault();
+  readLessLink_swiper.style.display = "none";
+  readMoreLink_swiper.style.display = "flex";
+});
+
+phoneButton.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    overlayPhone.classList.add("show");
+    overlayBurger.classList.remove("overlay__open");
+    overlay.classList.add("active");
+  });
+});
+
 burgerButton.addEventListener("click", () => {
   overlayBurger.classList.add("overlay__open");
   overlay.classList.add("active");
@@ -56,26 +96,26 @@ exitButton.addEventListener("click", () => {
   overlay.classList.remove("active");
 });
 
-phoneButton.addEventListener("click", () => {
-  overlayPhone.classList.add("show");
-  overlay.classList.add("active");
+exitPhone.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    overlayPhone.classList.remove("show");
+    overlay.classList.remove("active");
+  });
 });
 
-exitPhone.addEventListener("click", () => {
-  overlayPhone.classList.remove("show");
-  overlayBurger.classList.remove("overlay__open");
-  overlay.classList.remove("active");
+chatButton.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    overlayChat.classList.add("show");
+    overlayBurger.classList.remove("overlay__open");
+    overlay.classList.add("active");
+  });
 });
 
-chatButton.addEventListener("click", () => {
-  overlayChat.classList.add("show");
-  overlay.classList.add("active");
-});
-
-exitChat.addEventListener("click", () => {
-  overlayChat.classList.remove("show");
-  overlayBurger.classList.remove("overlay__open");
-  overlay.classList.remove("active");
+exitChat.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    overlayChat.classList.remove("show");
+    overlay.classList.remove("active");
+  });
 });
 
 readMoreLink.addEventListener("click", function (event) {
